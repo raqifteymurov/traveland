382618506fbfa2b2eb513f95bbeb816922154347
Raqif Teymurov <raqif.teymurov.585@teams.dia.edu.az>
2021-12-03 10:21:16 +0400

    slider

---
+++ Js/script.js
@@ -0,0 +1,25 @@
+$('.center').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 5,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
